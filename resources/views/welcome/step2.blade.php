e9cbc59767418f008dc2136c04aa58bc8578a741
Aleksei Sushkov <903434@gmail.com>
2020-02-01 21:06:16 +0100

    settings & step2 contacts fixes

--- resources/views/welcome/step2.blade.php
+++ resources/views/welcome/step2.blade.php
@@ -28,33 +28,62 @@
 @section('jscontent')
 <script>
 var i = 1;
-document.getElementById("add").addEventListener("click", function()
-{
-	var table = document.getElementById("dynamic_table");
-	var row = table.insertRow(-1);
+
+function deleteRow() {
+    var len = document.getElementsByTagName('tr').length;
+    var settingsNew = document.getElementById('settings__new');
+    if (len < 4){
+        settingsNew.classList.remove('settings__new_hidden')
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var len = document.getElementsByTagName("tr")
+    if (len == 4){
+        var row = document.getElementById("settings__new")
+        row.classList.add("settings__new_hidden")
+    }
+});
+
+document.getElementById("add").addEventListener("click", function(){
+    event.preventDefault();
+    var table = document.getElementById("dynamic_table");
+    var value = document.getElementById("new").value
+    var len = document.getElementsByTagName("tr")
+	var row = table.insertRow(len.length - 1);
 	row.id = "row" + i;
+
+    // Button column
+	var newCell2 = row.insertCell(-1);
+	var newButton = document.createElement("button");
+	newButton.id = i;
+	newButton.type = "button";
+    newButton.className = "settings__minus";
+	newButton.addEventListener("click", function() {
+		var rowToDelete = document.getElementById("row" + newButton.id);
+		table.deleteRow(rowToDelete.rowIndex);
+        deleteRow();
+	});
+	newCell2.appendChild(newButton);
 
 	// Button column
 	var newCell1 = row.insertCell(-1);
 	var newInput = document.createElement("input");
 	newInput.name = "name[]";
 	newInput.type = "text";
-	newInput.class = "form-control";
+    newInput.disabled = true;
+    newInput.value = value;
 	newCell1.appendChild(newInput);
 
-	// Button column
-	var newCell2 = row.insertCell(-1);
-	var newButton = document.createElement("button");
-	newButton.id = i;
-	newButton.type = "button";
-	newButton.innerHTML = '-';
-	newButton.addEventListener("click", function() {
-		var rowToDelete = document.getElementById("row" + newButton.id);
-		table.deleteRow(rowToDelete.rowIndex);
-	});
-	newCell2.appendChild(newButton);
-
 	i++;
+
+    var len = document.getElementsByTagName("tr").length;
+    var settingsNew = document.getElementById("settings__new");
+
+    if (len >= 4){
+        console.log('4');
+        settingsNew.classList.add("settings__new_hidden");
+    }
 });
 </script>
 @endsection
